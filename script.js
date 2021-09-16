3c67db927af143a07336b1c74b309754c68a6be8
gaeun58 <gaeun58@naver.com>
2021-09-16 23:25:06 +0900

    start 버튼

---
+++ script.js
@@ -0,0 +1,32 @@
+let minutes = 0;
+let seconds = 0;
+let tenMillis = 0;
+
+const appendTens = document.getElementById("tenMillis");
+const appendSeconds = document.getElementById("seconds");
+const appendMinutes = document.getElementById("minutes");
+const buttonStart = document.getElementById("bt_start");
+const buttonStop = document.getElementById("bt_stop");
+const buttonReset = document.getElementById("bt_reset");
+
+buttonStart.onclick = function (){
+    setInterval(operateTimer, 10)
+}
+
+// 10ms 마다 시간에 대한 숫자가 증가한다.
+function operateTimer() {
+    tenMillis++;
+    appendTens.textContent = tenMillis > 9 ? tenMillis : '0' + tenMillis
+    if(tenMillis > 99) {
+        seconds++;
+        appendSeconds.textContent = seconds > 9 ? seconds : '0' + seconds
+        tenMillis = 0
+        appendTens.textContent = "00"
+    }
+    if(seconds > 59) {
+        minutes++;
+        appendMinutes.textContent = minutes > 9 ? minutes : '0' + minutes
+        seconds = 0
+        appendSeconds.textContent = "00"
+    }
+}
